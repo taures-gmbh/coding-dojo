63e0aef7645c08db1d39a5393860416e9cfceba2
Kevin Grellus <k_grellus@taures.de>
2023-04-19 12:09:26 +0200

    use new jest config #boyscout

---
+++ tests/add.spec.ts
@@ -0,0 +1,15 @@
+import {add} from "../src/add";
+
+describe('testing add function', () => {
+    test('empty string should result in zero', () => {
+        expect(add('')).toBe(0);
+    });
+
+    test('negative number should throw error', () => {
+        expect(() => add('-1')).toThrow(RangeError);
+    });
+
+    test('1 + 2 should return correct value', () => {
+        expect(add('1,2')).toBe(3);
+    });
+});

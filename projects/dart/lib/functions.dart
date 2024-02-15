e6b7b41429c9b81e1b5ed6d383dc9d6895e61407
Jason T. Wilmans <j_wilmans@taures.de>
2024-02-15 11:26:38 +0100

    chore: init dart project correctly #boyscout

---
+++ projects/dart/lib/functions.dart
@@ -0,0 +1,3 @@
+String world() {
+  return "world";
+}
--- projects/dart/test/functions_test.dart
+++ projects/dart/test/functions_test.dart
@@ -1,8 +1,8 @@
-import 'package:dart/dart.dart';
+import 'package:dart/functions.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(world(), 'world');
   });
 }
